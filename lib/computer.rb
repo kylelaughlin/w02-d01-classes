0753c4e52c7d0b334e84df5d9ba9862897b8c332
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-21 16:40:36 -0500

    create computer class file

---
+++ lib/computer.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Computer
+
+def initialize(options={})
+  @brand = options[:brand]
+  @harddrive_size = options[:harddrive_size]
+  @screen_size = options[:screen_size]
+  @ram = options[:ram]
+  @processor_speed = option[:processor_speed]
+end
+
+end
