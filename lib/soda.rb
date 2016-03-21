eefa98245a988c41a619c9915b052aab2d0b922e
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-21 17:02:37 -0500

    Add soda class setters

--- lib/soda.rb
+++ lib/soda.rb
@@ -14,20 +14,39 @@ class Soda
     @brand
   end
 
+  def brand=(new_brand)
+    @brand = new_brand
+  end
+
   def calories
     @calories
   end
 
+  def calories=(new_calories)
+    @calories = new_calories
+  end
+
   def volume
     @volume
+  end
+  def volume=(new_volume)
+    @volume = new_volume
   end
 
   def sugar
     @sugar
   end
 
+  def sugar=(new_sugar)
+    @sugar = new_sugar
+  end
+
   def parent_company
     @parent_company
+  end
+
+  def parent_company=(new_parent_company)
+    @parent_company = new_parent_company
   end
   
 end
