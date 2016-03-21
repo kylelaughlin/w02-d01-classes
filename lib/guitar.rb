52b728ce2e795b1b7419927c1c6f23c73f0c919f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-21 17:26:06 -0500

    Create getters and setters for Guitar class

--- lib/guitar.rb
+++ lib/guitar.rb
@@ -9,5 +9,45 @@ class Guitar
     @color = options[:color]
     @string_brand = options[:string_brand]
   end
+
+  def model
+    @model
+  end
+
+  def model=(new_model)
+    @model = new_model
+  end
+
+  def brand
+    @brand
+  end
+
+  def brand=(new_brand)
+    @brand = new_brand
+  end
+
+  def pickup_type
+    @pickup_type
+  end
+
+  def pickup_type=(new_pickup_type)
+    @pickup_type = new_pickup_type
+  end
+
+  def color
+    @color
+  end
+
+  def color=(new_color)
+    @color = new_color
+  end
+
+  def string_brand
+    @string_brand
+  end
+
+  def string_brand=(new_string_brand)
+    @string_brand = new_string_brand
+  end
   
 end
