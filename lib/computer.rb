bd8550f02beb0ccbe74d056fbe6b7aec71339184
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-21 16:41:17 -0500

    Correct indentation

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 class Computer
 
-def initialize(options={})
-  @brand = options[:brand]
-  @harddrive_size = options[:harddrive_size]
-  @screen_size = options[:screen_size]
-  @ram = options[:ram]
-  @processor_speed = option[:processor_speed]
-end
+  def initialize(options={})
+    @brand = options[:brand]
+    @harddrive_size = options[:harddrive_size]
+    @screen_size = options[:screen_size]
+    @ram = options[:ram]
+    @processor_speed = option[:processor_speed]
+  end
 
 end
