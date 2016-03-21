404cd1ca99dfd4673eb8760546c333c0bd3f649f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-21 16:45:52 -0500

    Add computer class setters

--- lib/computer.rb
+++ lib/computer.rb
@@ -14,20 +14,40 @@ class Computer
     @brand
   end
 
+  def brand=(new_brand)
+    @brand = new_brand
+  end
+
   def harddrive_size
     @harddrive_size
+  end
+
+  def harddrive_size=(new_harddrive_size)
+    @harddrive_size = new_harddrive_size
   end
 
   def screen_size
     @screen_size
   end
 
+  def screen_size=(new_screen_size)
+    @screen_size = new_screen_size
+  end
+
   def ram
     @ram
   end
 
+  def ram=(new_ram)
+    @ram = new_ram
+  end
+
   def processor_speed
     @processor_speed
+  end
+
+  def processor_speed=(new_processor_speed)
+    @processor_speed = new_processor_speed
   end
 
 end
