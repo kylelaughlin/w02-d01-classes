5f672cfbf7d82fa2599feb2c57ab690572b3c396
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-22 17:29:45 -0500

    Set up Gemfile and Rakefile and test_helper; write computer tests

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,52 +2,14 @@ require 'pry'
 
 class Computer
 
+  attr_accessor :brand, :harddrive_size, :screen_size, :ram, :processor_speed
+
   def initialize(options={})
     @brand = options[:brand]
     @harddrive_size = options[:harddrive_size]
     @screen_size = options[:screen_size]
     @ram = options[:ram]
     @processor_speed = options[:processor_speed]
-  end
-
-  def brand
-    @brand
-  end
-
-  def brand=(new_brand)
-    @brand = new_brand
-  end
-
-  def harddrive_size
-    @harddrive_size
-  end
-
-  def harddrive_size=(new_harddrive_size)
-    @harddrive_size = new_harddrive_size
-  end
-
-  def screen_size
-    @screen_size
-  end
-
-  def screen_size=(new_screen_size)
-    @screen_size = new_screen_size
-  end
-
-  def ram
-    @ram
-  end
-
-  def ram=(new_ram)
-    @ram = new_ram
-  end
-
-  def processor_speed
-    @processor_speed
-  end
-
-  def processor_speed=(new_processor_speed)
-    @processor_speed = new_processor_speed
   end
 
   # Upgrades the ram by the integer passed as a parameter and puts the new ram amount
@@ -63,4 +25,4 @@ class Computer
 
 end
 
-binding.pry
+#binding.pry
